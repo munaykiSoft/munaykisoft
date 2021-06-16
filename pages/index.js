1e452d010bda3c75ec7078882f88d8a7466b173b
RamirezJosue <xdemobtkx@gmail.com>
2021-06-16 17:24:28 -0500

    Button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import { Button } from '../components/components/Button';
 
 
 export default function Home() {
   return (
     <>
       Pagina inicial
+      <Button size="base" text="Primary" type="secondary" />
+      {/* <Button size="sm" text="Primary" />
+      <Button size="base" text="Primary" />
+      <Button size="lg" text="Primary" />
+      <Button size="xl" text="Primary" /> */}
     </>
   )
 }
