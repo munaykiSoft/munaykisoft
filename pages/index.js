8de20e2e5019d747cb61a55aca978a3a1b9e4d33
RamirezJosue <xdemobtkx@gmail.com>
2021-06-21 16:58:45 -0500

    Clientes

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,53 @@
 import { useEffect, useState } from "react"
+import { getClientsApi } from "../api/clients"
 import { getServicesApi } from "../api/services"
 
 export default function Home() {
 
   const [services, setServices] = useState([])
+  const [clients, setClients] = useState([])
 
   useEffect(() => {
     (
       async () => {
         const response = await getServicesApi();
         setServices(response || []);
-        console.log(response);
+        console.log('goooooooooo', response);
+      }
+    )()
+  }, []);
+
+  useEffect(() => {
+    (
+      async () => {
+        const response = await getClientsApi();
+        setClients(response || []);
+        console.log('gaaaaaaaaaaaa',response);
       }
     )()
   }, [])
   return (
     <>
-      Pagina inicial
+      <ul>
+        {
+          services.map((service) => (
+            <li key={service.id}>
+              { service.nombre }
+            </li>
+          ))
+        }
+      </ul>
       <br/>
-      {services[0].nombre}
       <br/>
-      <img src={services[0].imagen[0].url} alt="imagen" />
-      Pagina inicial
       <br/>
-      Pagina inicial
+      {
+        clients.map((client) => 
+          <div key={ client.id }>
+            <img src={ client.imagen.url} alt={client.imagen.name}/>
+          </div>
+        )
+      }
       <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
-      <br/>
-      Pagina inicial
       <br/>
     </>
   )
